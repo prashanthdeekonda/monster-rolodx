b1cd5c285f5dc82d6830aef88cae908f38144f08
Prashanth Deekonda <prashanth.deekonda@gmail.com>
2024-02-09 21:45:00 -0500

    set state

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Prashanth",
+      company: "ZTM",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name}, I work at {this.state.company}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button
+            onClick={(e) => {
+              this.setState({ name: "suchi" });
+            }}
           >
-            Learn React
-          </a>
+            Change Name
+          </button>
         </header>
       </div>
     );
