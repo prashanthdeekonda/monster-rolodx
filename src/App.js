baed5db8c4f979f8de133afc413180bd88993285
Prashanth Deekonda <prashanth.deekonda@gmail.com>
2024-02-13 17:21:37 -0500

    using map

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Prashanth",
-      company: "ZTM",
+      monsters: [
+        { name: "Linda", id: "1" },
+        { name: "Frank", id: "2" },
+        { name: "Jacky", id: "3" },
+        { name: "Andrei", id: "4" },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}, I work at {this.state.company}
-          </p>
-          <button
-            onClick={(e) => {
-              this.setState(
-                () => {
-                  return {
-                    name: "suchi",
-                  };
-                },
-                () => {
-                  // optional callback
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
