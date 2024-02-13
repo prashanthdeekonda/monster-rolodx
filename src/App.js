696430772702e83b0ac9f220c77e0f36535a7fa1
Prashanth Deekonda <prashanth.deekonda@gmail.com>
2024-02-13 18:07:42 -0500

    fetch users

--- src/App.js
+++ src/App.js
@@ -4,17 +4,31 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      monsters: [
-        { name: "Linda", id: "1" },
-        { name: "Frank", id: "2" },
-        { name: "Jacky", id: "3" },
-        { name: "Andrei", id: "4" },
-      ],
+      monsters: [],
     };
+    console.log("1");
+  }
+
+  componentDidMount() {
+    console.log("3");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
+    console.log("2 ");
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
