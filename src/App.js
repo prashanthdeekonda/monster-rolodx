edbc438ab9b2378035baab4264abde6afef18155
Prashanth Deekonda <prashanth.deekonda@gmail.com>
2024-02-14 20:25:05 -0500

    monsterlist

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
-    console.log("1");
+    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log("3");
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -28,9 +28,32 @@ class App extends Component {
   }
 
   render() {
-    console.log("2 ");
+    console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={(event) => {
+            console.log("startingarray:", this.state.monsters);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+            
+            const monsterList = this.state.monsters;
+
+            this.setState(
+              () => {
+                return { monsters: filteredMonsters };
+              },
+              () => {
+                console.log("endingarray:", this.state.monsters);
+              }
+            );
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
