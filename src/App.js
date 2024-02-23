4b4fb2c3c8ce9b6557f67b81a15a11495eccacae
Prashanth Deekonda <prashanth.deekonda@gmail.com>
2024-02-22 23:02:28 -0500

    convert to functional components

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const App = () => {
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
+  console.log('rendered');
+  
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,31 +4,38 @@
 // index.js
 // based on your coding standards set by the team
 
-import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-          // return (
-          //   <div className="card-container" key={id}>
-          //     <img
-          //       alt={`monster ${name}`}
-          //       src={`https://robohash.org/${id}?set=set2&size=180x180`}
-          //     />
-          //     <h2>{name}</h2>
-          //     <p>{email}</p>
-          //   </div>
-          // );
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster}  key={monster.id} />;
+    })}
+  </div>
+);
+
+// class CardList extends Component {
+//   render() {
+//     const { monsters } = this.props;
+//     return (
+//       <div className="card-list">
+//         {monsters.map((monster) => {
+//           return <Card monster={monster} />;
+//           // return (
+//           //   <div className="card-container" key={id}>
+//           //     <img
+//           //       alt={`monster ${name}`}
+//           //       src={`https://robohash.org/${id}?set=set2&size=180x180`}
+//           //     />
+//           //     <h2>{name}</h2>
+//           //     <p>{email}</p>
+//           //   </div>
+//           // );
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
